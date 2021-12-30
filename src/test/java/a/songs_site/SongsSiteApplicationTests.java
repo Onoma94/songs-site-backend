f33c60635a2b2567e5a4050e330cd81ea2e482d8
Onoma94 <artur.gierach@wp.pl>
2021-12-30 01:22:16 +0100

    s

---
+++ src/test/java/a/songs_site/SongsSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package a.songs_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
