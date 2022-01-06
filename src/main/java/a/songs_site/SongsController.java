48c5b3547c770f60ddac7963309c42a0e5fae5dd
Onoma94 <artur.gierach@wp.pl>
2022-01-06 22:36:34 +0100

    CrossOrigin to solve 403 CORS error

--- src/main/java/a/songs_site/SongsController.java
+++ src/main/java/a/songs_site/SongsController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*", allowedHeaders="*")
 @RestController
 @RequestMapping("/api")
 public class SongsController 
